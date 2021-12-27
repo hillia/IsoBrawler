f3c175f0b55824f44b74fa27c78a0d3154a51207
Ian Hill <khaemera@gmail.com>
2021-12-26 18:20:38 -0600

    Initial commit for IsoBrawler

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    public float speed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float h = Input.GetAxisRaw("Horizontal");
+        float v = Input.GetAxisRaw("Vertical");
+
+        gameObject.transform.position =
+            new Vector3(
+                transform.position.x + (h * speed),
+                transform.position.y,
+                transform.position.z + (v * ZSpeed())
+            );
+    }
+
+    float ZSpeed() {
+        return speed;
+    }
+}
